ec6c2b426a455df301e9249e559ea522320980d1
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-09-03 21:30:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let FirstName = prompt ("Ismingizni kiriting;"),
+LastName = prompt ("Familiyangizni kiriting;"),
+age = prompt ("Yoshingizni kiriting;"),
+sub = prompt ("Yoqtirgan faningizni kiriting;"),
+manzil = prompt ("Manzilingizni kiriting;")
+
+
+console.log("Ism",FirstName);
+console.log("Familiya",LastName);
+console.log("Yoshingiz",age);
+console.log("Yoqtirgan faningiz",sub);
+console.log("Manzil",manzil);
